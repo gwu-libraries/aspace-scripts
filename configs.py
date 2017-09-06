892dfdfc6f9e6aa93439bdfa410bc637f9042534
reliztrent <racheltren@gmail.com>
2017-09-06 10:13:43 -0400

    configuration files

---
+++ configs.py
@@ -0,0 +1,14 @@
+#Repository ID for digital object and archival object records. 
+#Directions: replace 0 with the digit ID of your intended archivesspace repository
+repositoryID = '0'
+
+#Input file path for new digital objects. 
+#Directions: only use if necessary for your python script
+digital_object_csv = 'path\to\file_with_metadata_about_NEW_objects.csv'
+
+#Input file path for updating digital objects
+#Directions: only use if necessary for your python script
+digital_object_update_csv = 'path\to\file_with_edited_metadata_about_EXISTING_objects.csv'
+
+#Output file path
+updated_digital_object_csv  = 'path\to\file_to_record_output.csv'
---
+++ secrets.py
@@ -0,0 +1,8 @@
+#API baseURL of archivesspace (probably ends in :8089)
+baseURL='"
+
+#Username to execute commands
+user=''
+
+#Password for user
+password=''
